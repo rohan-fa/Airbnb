0f9db06aba709bd320d24db84b055fe1c4e7d83a
rohan-fa <rohanfarzana@gmail..com>
2023-12-13 23:03:36 +1100

    layout, modal

--- airbnb/app/components/modals/Modal.tsx
+++ airbnb/app/components/modals/Modal.tsx
@@ -1,6 +1,30 @@
 
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryActionLabel?: string;
+}
 
-const Modal = () => {
+const Modal: React.FC<ModalProps> = ({
+    isOpen, 
+    onClose, 
+    onSubmit, 
+    title, 
+    body, 
+    actionLabel, 
+    footer, 
+    disabled,
+    secondaryAction,
+    secondaryActionLabel
+
+}) => {
     return(
         <div>
             modal
--- airbnb/app/layout.tsx
+++ airbnb/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Navbar />
-        <Modal/>
+        <Modal />
         {children}
       </body>
     </html>

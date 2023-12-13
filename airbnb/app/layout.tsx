16e70bd1888be2a647942ead146fdac00aad7416
rohan-fa <rohanfarzana@gmail..com>
2023-12-13 22:57:24 +1100

    AuthUI

--- airbnb/app/layout.tsx
+++ airbnb/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar';
+import Modal from './components/modals/Modal';
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Navbar />
+        <Modal/>
         {children}
       </body>
     </html>

5f3ca69856eaf2f4dd54bb4db7a5d8db94a0a2cd
rohan-fa <rohanfarzana@gmail..com>
2023-12-15 19:06:48 +1100

    modified modal.tsx

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -37,14 +37,34 @@ const Modal: React.FC<ModalProps> = ({
     //check whether modal is disable, if disable we are going to break the function not allow anything happen once we click on the close button. otherwise turn our local setModal to false and open a timeout
     const handleClose = useCallback(() => {
         if(disabled){
-            return;
+            return;     //break the fnctn not allow anything to happen once click on the close button. now turn our local set model to false
         }
         setShowModal(false);
         setTimeout(() => {
-            onClose();
+            onClose();      // adding onClose here cz will add some animation. to delay onClose
         }, 300);
     }, [disabled, onClose]);    
 
+    //Submit
+    const handleSubmit = useCallback(() => {
+        if(disabled){
+            return;
+        }
+        onSubmit()
+    }, [disabled, onSubmit]);
+
+    //Now with the secondary action, button
+    const handleSecondaryAction = useCallback(() => {
+        if (disabled || !secondaryAction ){
+            return;
+        }
+        //its not disabled and have secondaryAction
+        secondaryAction();
+    },[disabled, secondaryAction]);
+
+        if(!isOpen){
+            return null;
+        }
 
     return(
         <div>
